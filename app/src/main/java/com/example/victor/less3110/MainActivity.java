f909ce19db980e939d503de15a5785f6d1e75af1
sedworld <sedworld@mail.ru>
2016-11-02 20:44:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/victor/less3110/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.victor.less3110;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.victor.less3110.adapters.NotesAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView = null;
+    private Toolbar toolbar = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.list_notes);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(R.string.app_name);
+
+        //как будет отображать список
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false); //вертикальный список
+        recyclerView.setLayoutManager(layoutManager);
+        NotesAdapter adapter = new NotesAdapter();
+        List<String> dataSource = new ArrayList<String>();
+        for(int i = 0; i < 100; i++){
+            dataSource.add("title: " + i);
+        }
+        recyclerView.setAdapter(adapter);
+        adapter.setDataSource(dataSource);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_settings:
+                Snackbar.make(recyclerView,R.string.action_settings,Snackbar.LENGTH_SHORT).show();
+                return true;
+            case R.id.help:
+                Snackbar.make(recyclerView,R.string.help,Snackbar.LENGTH_SHORT).show();
+                return true;
+            default: return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+}
